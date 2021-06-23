e03ae356c8d39b3872a90994cc539c8ff0595ed5
Sergey Rozhkov <borzsonite@gmail.com>
2021-06-23 13:33:23 +0300

    Hello controller add

---
+++ src/main/java/edu/segeyrozhkov/crud/app/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package edu.segeyrozhkov.crud.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping()
+    public String getHello() {
+        return "hello";
+    }
+}
