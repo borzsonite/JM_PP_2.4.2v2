81ec2b0952a456e3b48490c62a289ceb0e800d60
Sergey Rozhkov <borzsonite@gmail.com>
2021-06-24 13:51:21 +0300

    getUsersList mapping add

--- src/main/java/edu/segeyrozhkov/crud/app/controllers/UserController.java
+++ src/main/java/edu/segeyrozhkov/crud/app/controllers/UserController.java
@@ -4,6 +4,7 @@ import edu.segeyrozhkov.crud.app.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -15,8 +16,16 @@ public class UserController {
     public void setUserService(UserService userService) {
         this.userService = userService;
     }
+
     @GetMapping()
     public String getHello() {
         return "hello";
+    }
+
+    @GetMapping("/users")
+    public String getAllUsers(Model model) {
+        model.addAttribute("usersList", userService.userList());
+        System.out.println(userService.userList());
+        return "users";
     }
 }
