7718896f1e7ce6145dacc0f734f22a56e11bc762
Sergey Rozhkov <borzsonite@gmail.com>
2021-07-02 11:46:27 +0300

    create table issue resolved

--- src/main/java/edu/segeyrozhkov/crud/app/config/SpringConfig.java
+++ src/main/java/edu/segeyrozhkov/crud/app/config/SpringConfig.java
@@ -21,7 +21,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
-import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Properties;
 
